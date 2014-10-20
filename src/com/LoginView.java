700d46a4253e2e925bee40f0fdde70fca3a1e4e2
MaciejGawel <m.gawel@hotmail.com>
2014-10-20 21:45:00 +0200

    Add basic login view

--- src/com/LoginView.java
+++ src/com/LoginView.java
@@ -1,59 +1,94 @@
 package com;
 
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.Page;
+import com.vaadin.server.Responsive;
+import com.vaadin.shared.Position;
 import com.vaadin.ui.*;
-import com.vaadin.ui.themes.Reindeer;
+import com.vaadin.ui.themes.ValoTheme;
 
 import javax.swing.*;
 
 /**
  * Created by Maciej on 2014-10-18.
  */
-public class LoginView extends CustomComponent implements View, Button.ClickListener {
+public class LoginView extends VerticalLayout implements View {
 
     public static final String NAME = "login";
-    private final TextField user;
-    private final Button loginButton;
+//    private final TextField user;
+//    private final Button loginButton;
 
     public LoginView() {
         setSizeFull();
 
-        // Create thr user input field
-        user = new TextField("Name:");
-        user.setRequired(true);
-        user.setInputPrompt("Your unique username");
+        Component loginForm = buildLoginForm();
+        addComponent(loginForm);
+        setComponentAlignment(loginForm, Alignment.MIDDLE_CENTER);
 
-        // Create login button
-        loginButton = new Button("Login", this);
+        Notification notification = new Notification("Welcome to the Gra w kosci");
+        notification.setHtmlContentAllowed(true);
+        notification.setStyleName("tray dark small closable login-help");
+        notification.setPosition(Position.BOTTOM_CENTER);
+        notification.show(Page.getCurrent());
+    }
 
-        // Add to a panel
-        VerticalLayout fields = new VerticalLayout(user, loginButton);
-        fields.setCaption("Please enter username to access the application.");
-        fields.setStyleName(Reindeer.LABEL_H1);
+    private Component buildLoginForm() {
+        final VerticalLayout loginPanel = new VerticalLayout();
+        loginPanel.setSizeUndefined();
+        loginPanel.setSpacing(true);
+        Responsive.makeResponsive(loginPanel);
+//        loginPanel.addStyleName("login-panel");
+
+        loginPanel.addComponent(buildLabels());
+        loginPanel.addComponent(buildFields());
+        return loginPanel;
+    }
+
+    private Component buildFields() {
+        HorizontalLayout fields = new HorizontalLayout();
         fields.setSpacing(true);
-        fields.setMargin(new MarginInfo(true, true, true, false));
-        fields.setSizeUndefined();
+//        fields.addStyleName("fields");
 
-        // The view root layout
-        VerticalLayout viewLayout = new VerticalLayout(fields);
-        viewLayout.setSizeFull();
-        viewLayout.setComponentAlignment(fields, Alignment.MIDDLE_CENTER);
-        viewLayout.setStyleName(Reindeer.LAYOUT_BLUE);
-        setCompositionRoot(viewLayout);
+        final TextField username = new TextField("username");
+        username.setIcon(FontAwesome.USER);
+        username.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
+
+        final Button signin = new Button("Sign In");
+        signin.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        signin.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+        signin.focus();
+
+        fields.addComponents(username, signin);
+        fields.setComponentAlignment(signin, Alignment.BOTTOM_LEFT);
+
+        signin.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                getSession().setAttribute("user", username.getValue());
+                getUI().getNavigator().navigateTo(LoginMainView.NAME);
+            }
+        });
+        return fields;
+    }
+
+    private Component buildLabels() {
+        CssLayout labels = new CssLayout();
+//        labels.addStyleName("labels");
+
+        Label welcome = new Label("Welcome");
+        welcome.setSizeUndefined();
+        welcome.addStyleName(ValoTheme.LABEL_H4);
+        welcome.addStyleName(ValoTheme.LABEL_COLORED);
+        labels.addComponent(welcome);
+
+        return labels;
     }
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
-        // focus the username field when user arrives to the login view
-        user.focus();
-    }
 
-    @Override
-    public void buttonClick(Button.ClickEvent event) {
-        String username = user.getValue();
-        getSession().setAttribute("user", username);
-        getUI().getNavigator().navigateTo(LoginMainView.NAME);
     }
 }
